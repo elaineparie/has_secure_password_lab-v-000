1a5462e708d0e219d14dbb6196dfc6948b8b8ded
Elaine Anderson <eparie@bu.edu>
2018-08-13 12:47:18 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,9 @@
 class SessionsController < ApplicationController
   def create
     @user = User.find_by(name: params[:user][:name])
-   if @user
-     session[:user_id] = @user.id
     return head(:forbidden) unless @user.authenticate(params[:password])
-  
+    session[:user_id] = @user.id
     redirect_to '/'
-  end
 
     end
 

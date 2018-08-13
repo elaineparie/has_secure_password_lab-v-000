f74a2774c2502a9bcf03c2156d50a45faba95ae0
Elaine Anderson <eparie@bu.edu>
2018-08-13 12:59:28 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
     if @user.authenticate(params[:password])
     session[:user_id] = @user.id
     redirect_to '/'
-  else 
+  else
     redirect_to '/login'
 
     end

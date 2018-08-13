d9f6e95811ec12630a2bb7ab123f2f73f36cfb5b
Elaine Anderson <eparie@bu.edu>
2018-08-13 12:46:49 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,10 +3,8 @@ class SessionsController < ApplicationController
     @user = User.find_by(name: params[:user][:name])
    if @user
      session[:user_id] = @user.id
-   end
     return head(:forbidden) unless @user.authenticate(params[:password])
-    session[:user_id] = @user.id
-    binding.pry
+  
     redirect_to '/'
   end
 

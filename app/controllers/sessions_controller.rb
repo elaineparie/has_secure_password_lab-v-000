50acb2a3a52207fd4e5de7e43ff5df883b78b598
Elaine Anderson <eparie@bu.edu>
2018-08-13 12:28:00 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,5 @@
 class SessionsController < ApplicationController
   def create
-    binding.pry
     @user = User.find_by(name: params[:user][:name])
   #  if @user
     unless @user.authenticate(params[:password])
